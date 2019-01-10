7f167bdcfcb081908fa0209f14bb1477fe2a62b8
Gideon Pfeffer <gap16@duke.edu>
2019-01-10 11:37:05 -0500

    adds delete

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -25,15 +25,23 @@ class ArticlesController < ApplicationController
 		end
 	end
 
-def update
-	@article = Article.find(params[:id])
+	def update
+		@article = Article.find(params[:id])
 
-	if @article.update(article_params)
-		redirect_to @article
-	else
-		render 'edit'
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
-end
+
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
 
 private
 	def article_params
